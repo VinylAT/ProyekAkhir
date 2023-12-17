59a0f5c29557389d8c6dd2bee7367c6d1d1ea953
Guntur <gunturguritno@gmail.com>
2023-12-17 10:51:40 +0700

    AttendeeEditActivity is done

--- app/src/main/java/com/polinema/proyekakhir/AttendeeEditActivity.java
+++ app/src/main/java/com/polinema/proyekakhir/AttendeeEditActivity.java
@@ -4,12 +4,17 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.List;
@@ -18,7 +23,7 @@ public class AttendeeEditActivity extends AppCompatActivity {
 // Todo : Add declaration, and the edit functions
     private EditText editText_Name, editText_NI;
     private DatabaseReference databaseAttendee;
-    private String currentSessionID;
+    private String currentAttendeeID;
     private ListView listViewAttendee;
     private List<Attendee> listAttendee;
     @Override
@@ -27,7 +32,8 @@ public class AttendeeEditActivity extends AppCompatActivity {
         setContentView(R.layout.activity_attendee_edit);
         editText_Name = findViewById(R.id.nameEdit);
         editText_NI = findViewById(R.id.niEdit);
-
+        currentAttendeeID = getIntent().getStringExtra("AttendeeID");
+        databaseAttendee = FirebaseDatabase.getInstance().getReference("attendee").child(currentAttendeeID);
     }
     @Override
     protected void onStart(){
@@ -35,12 +41,45 @@ public class AttendeeEditActivity extends AppCompatActivity {
         databaseAttendee.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-
+                Attendee attendee = snapshot.getValue(Attendee.class);
+                if (attendee != null){
+                    String nama = attendee.getAttendee_Nama();
+                    String NI = attendee.getAttendee_NI();
+                    editText_Name.setText(nama);
+                    editText_NI.setText(NI);
+                }
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
 
+            }
+        });
+    }
+
+    public void buttonupdateAttendee(View view) {
+        String nama = editText_Name.getText().toString();
+        String ni = editText_NI.getText().toString();
+        if (!TextUtils.isEmpty(nama) && !TextUtils.isEmpty(ni)){
+            Attendee attendee = new Attendee(currentAttendeeID, nama, ni);
+            databaseAttendee.setValue(attendee).addOnSuccessListener(this, new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void unused) {
+                    // Toast popup
+                    Toast.makeText(AttendeeEditActivity.this, "Attendee data updated", Toast.LENGTH_LONG).show();
+                    finish();
+                }
+            });
+        } else {
+            Toast.makeText(this, "Please fill all forms", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void buttondeleteAttendee(View view) {
+        databaseAttendee.removeValue().addOnSuccessListener(this, new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                Toast.makeText(AttendeeEditActivity.this, "Attendee removed", Toast.LENGTH_LONG).show();
             }
         });
     }
